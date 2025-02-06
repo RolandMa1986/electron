778448a477d188efcd876d2c5a3f06b2f5daef0a
Electron Builder <electron_builder@email.com>
2025-02-06 10:38:26 +0800

    win7

--- shell/common/language_util_win.cc
+++ shell/common/language_util_win.cc
@@ -18,6 +18,8 @@ namespace electron {
 
 bool GetPreferredLanguagesUsingGlobalization(
     std::vector<std::wstring>* languages) {
+  if (base::win::GetVersion() < base::win::Version::WIN10)
+    return false;
   base::win::ScopedHString guid = base::win::ScopedHString::Create(
       RuntimeClass_Windows_System_UserProfile_GlobalizationPreferences);
   Microsoft::WRL::ComPtr<
